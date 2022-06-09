f35b3c53323f9ac8b188ec60b959d31beb800d65
kenny <kenny397@naver.com>
2022-06-09 15:05:01 +0900

    feat : BaseResponse 추가

---
+++ task/task/src/main/java/com/ssg/task/api/dto/response/BaseResponseBody.java
@@ -0,0 +1,18 @@
+package com.ssg.task.api.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class BaseResponseBody {
+    String message= null;
+    Integer statusCode = null;
+
+    public BaseResponseBody() {}
+
+    public static BaseResponseBody of(Integer statusCode, String message) {
+        BaseResponseBody body = new BaseResponseBody();
+        body.message = message;
+        body.statusCode = statusCode;
+        return body;
+    }
+}
